3f75025d80e5cf56c15eed921871313a5a1c3f01
niko.yue <niko.yue3@gmail.com>
2019-06-16 18:00:38 +0800

    LRU

--- practice6.c
+++ practice6.c
@@ -5,16 +5,14 @@
 
 #define F 4
 #define P 10
-#define INT_ARRAY_SIZE(x, type) (sizeof(x)/sizeof(type))
 
 void FIFO(int*);
 void LRU(int*);
 int* generate_RS();
 
 typedef struct practice6_t {
-    int mtime;
+    int prior;//in FIFO first in will get small number; in LRU is used be time
     int m;
-    int prior;//first in will get small number
 } PHY_MEM, *PHY_MEMP;
 PHY_MEMP quicksort(PHY_MEMP a, int low, int high);
 
@@ -42,18 +40,21 @@ void FIFO(int *rs)
 
     for (int rs_index = 0; rs_index < P; rs_index++) {
         for (int f = 0; f < F; f++) {
-            if (ddr[f].m == rs[rs_index]) {
-                goto skip;
-            }
             if (ddr[f].prior == -1) {
                 ddr[f].m = rs[rs_index];
                 ddr[f].prior = rs_index;
                 goto skip;
+            } else if (ddr[f].m == rs[rs_index]) {
+                goto skip;
+            } else {
+                continue;
             }
         }
 
-        quicksort(ddr, 0, F - 1);//ddr.prior now example 0 1 2 3
-        ddr[0].prior = ddr[F-1].prior + 1;//ddr.prior now example 3+1 1 2 3
+        printf("missing rs = %d\n", rs[rs_index]);
+        quicksort(ddr, 0, F - 1);//return ddr.prior now example 0 1 2 3
+        printf("remove = %d\n", ddr[0].m);
+        ddr[0].prior = ddr[F - 1].prior + 1;//ddr.prior now example 3+1 1 2 3
         ddr[0].m = rs[rs_index];
         page_swap_count++;
 
@@ -61,12 +62,55 @@ skip:
         continue;
     }
 
-    printf("rate of page missing %lf", (double)page_swap_count / (double)P);
+    printf("FIFO rate of page missing %lf", (double)page_swap_count / (double)P);
     free(rs);
 }
 
 void LRU(int *rs)
 {
+    int page_swap_count = 0;
+
+    PHY_MEM ddr[F]; 
+    for (int i = 0; i < F; i++) {
+         ddr[i].m = -1;
+         ddr[i].prior = 0;
+    }
+
+    for (int rs_index = 0; rs_index < P; rs_index++) {
+        for (int f = 0; f < F; f++) {
+            if (ddr[f].prior == 0) {
+                ddr[f].m = rs[rs_index];
+                for (int i = 0; i <= f; i++) {
+                    ddr[i].prior++;
+                }
+                goto skip;
+            } else if (ddr[f].m == rs[rs_index]) {
+                ddr[f].prior = 1;
+                for (int i = 0; i < F; i++) {
+                    if (i != f && ddr[i].prior != 0) {
+                        ddr[i].prior++;
+                    }
+                }
+                goto skip;
+            } else {
+                continue;
+            }
+        }
+        printf("missing rs = %d\n", rs[rs_index]);
+        quicksort(ddr, 0, F - 1);//ddr.prior now example 1 2 3 4
+        printf("remove = %d\n", ddr[F -1].m);
+        ddr[F - 1].prior = 1;
+        ddr[F - 1].m = rs[rs_index];
+        for (int i = 0; i < F - 1; i++) {
+            ddr[i].prior++;
+        }
+        page_swap_count++;
+
+skip:
+        continue;
+    }
+
+    printf("LRU rate of page missing %lf", (double)page_swap_count / (double)P);
     free(rs);
 }
 
@@ -75,6 +119,7 @@ int* generate_RS()
     int range = P * sizeof(int);
     int *rs = (int*)malloc(range);
     srand(time(NULL));
+    printf("rs: ");
     for (int i = 0; i < 10; i++) {
         rs[i] = rand() % 10;//random 0~9
         printf("%d ", rs[i]);
@@ -122,4 +167,3 @@ PHY_MEMP quicksort(PHY_MEMP ddr, int low, int high)
 
     return ddr;
 }
-
